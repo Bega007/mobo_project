a591dab3d9443ed0002d83f3dd21b7105401edf1
Bega007 <begajan007@gmail.com>
2023-12-10 05:38:45 +0500

    v1.0.3

---
+++ lib/screens/my_all_markets_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:mobo_project/utils/constants/my_sizes.dart';
+import 'package:mobo_project/widgets/my_appbar.dart';
+import 'package:mobo_project/widgets/my_grid_view.dart';
+import 'package:mobo_project/widgets/my_market_card.dart';
+import 'package:mobo_project/widgets/my_market_products.dart';
+import 'package:mobo_project/widgets/my_section_heading.dart';
+
+class MyAllMarketsScreen extends StatelessWidget {
+  const MyAllMarketsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const MyAppBar(
+        showBackArrow: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(MySizes.defaultSpace),
+          child: Column(
+            children: [
+              // Heading
+              const MySectionHeading(
+                title: 'Markets',
+                showActionButton: false,
+              ),
+
+              const SizedBox(
+                height: MySizes.spaceBtwItems,
+              ),
+
+              //Brands Markets
+              MyGridView(
+                  itemCount: 10,
+                  mainAxisExtent: 80,
+                  itemBuilder: (context, index) => MyMarketCard(
+                        showBorder: true,
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const MyMarketProducts()),
+                          );
+                        },
+                      )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
